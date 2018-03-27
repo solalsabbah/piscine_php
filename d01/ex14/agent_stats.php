7fe65f600bb4d3637f0ce9caeb27040d86ae1b53
solalsabbah <solal.sabbah@gmail.com>
2018-03-27 16:28:45 +0200

    d01 finished

--- d01/ex14/agent_stats.php
+++ d01/ex14/agent_stats.php
@@ -2,6 +2,11 @@
 <?PHP
 	$tab = array();
 	$user_mean = array();	
+	$array_moul = array();
+
+
+	if ($argc != 2) 
+		exit ;
 
 	while ($line = fgets(STDIN))
 	{
@@ -10,6 +15,12 @@
 			array_push($tab, $line);
 		}
 	}
+
+	$moyenne = 0;
+	$div_moy = 0;
+	$note_m = 0;
+	$count_moul = 0;
+	$tot_moul = 0;
 	foreach($tab as $elem)
 	{
 		preg_match("#^[a-zA-Z]+_[a-zA-Z]+#", $elem, $match);
@@ -18,23 +29,49 @@
 		$div = 0;
 		foreach($tab as $i => $row)
 		{
+
 			preg_match("#^[a-zA-Z]+_[a-zA-Z]+#", $row, $a);
 			if ($name == $a[0])
 			{
 				preg_match("#;([0-1]?[0-9]|20);#", $row, $mean);
 				$tot =  str_replace(";", "", $mean[0])."\n" + $tot;
 				$div++;
+
+				if (preg_match("#;moulinette;#", $row))
+				{
+					preg_match("#;([0-1]?[0-9]|20);#", $row, $moul_mean);
+					$note_moul = str_replace(";", "", $moul_mean[0]);		
+					$tot_moul = $note_moul + $tot_moul;
+					$count_moul++;
+				}
 				unset($tab[$i]);
 			}
-		}		
+		}	
+		$moyenne = ($tot + $moyenne);
+		$div_moy = $div + $div_moy;
+		
 		if ($div != 0)
 		{
-			array_push($user_mean, $name.":".($tot / $div)."\n");
-		}
+			array_push($user_mean, $name.":".($tot - $note_moul) / ($div - 1)."\n");
+			array_push($array_moul, $name.":".((- $note_moul + ($tot - $note_moul) / ($div - 1))."\n"));
+		}	
 	}
-	sort($user_mean);
-	foreach ($user_mean as $x)
-		echo $x;
 
-	// need to work on global mean
+	$moyenne -= $tot_moul;
+	$div_moy -= $count_moul;
+	
+	if ($argv[1] == "moyenne_user")
+	{
+		sort($user_mean);
+		foreach ($user_mean as $x)
+			echo $x;
+	}	
+	if ($argv[1] == "moyenne")
+		print($moyenne / $div_moy."\n");
+	if ($argv[1] == "ecart_moulinette")
+	{
+		sort($array_moul);
+		foreach ($array_moul as $x)
+			echo $x;
+	}
 ?>
