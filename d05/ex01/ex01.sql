fe2e3befa7d50c05e203398aa29cbd6b08699154
solalsabbah <solal.sabbah@gmail.com>
2018-04-02 19:02:37 +0200

    d05 starting sql

---
+++ d05/ex01/ex01.sql
@@ -0,0 +1,1 @@
+CREATE TABLE ft_table(id INT NOT NULL AUTO_INCREMENT, login VARCHAR(9) NOT NULL DEFAULT 'toto', `group` ENUM('staff', 'student', 'other') NOT NULL, creation_date DATE NOT NULL, PRIMARY KEY (id));
