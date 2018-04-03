333f84fe241972cba86b4228ba9bd8a248c279ef
solalsabbah <solal.sabbah@gmail.com>
2018-04-03 19:31:09 +0200

    ex18 changed

--- d05/ex18/ex18.sql
+++ d05/ex18/ex18.sql
@@ -1,1 +1,1 @@
-SELECT name, id_distrib FROM distrib WHERE name REGEXP 'a.*a|Y.*Y' AND (id_distrib = 42 OR id_distrib = 71 OR id_distrib BETWEEN 62 AND 69 OR id_distrib BETWEEN 88 AND 90) LIMIT 5 OFFSET 2;
+SELECT name, id_distrib FROM distrib WHERE name REGEXP 'y.*y|Y.*Y' AND (id_distrib = 42 OR id_distrib = 71 OR id_distrib BETWEEN 62 AND 69 OR id_distrib BETWEEN 88 AND 90) LIMIT 5 OFFSET 2;
